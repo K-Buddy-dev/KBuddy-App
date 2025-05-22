182d0a1dc44121c56bfe0e3f918abb9666bc10da
parkjongjoon7128 <whdwnsdk8111@naver.com>
2025-05-22 19:33:11 +0900

    feat: 키보드 상태에 따른 높이값 구현 및 웹뷰로 데이터 전송 구현

--- src/screens/WebViewScreen.tsx
+++ src/screens/WebViewScreen.tsx
@@ -5,6 +5,9 @@ import React, { useEffect, useRef, useState } from "react";
 import {
   BackHandler,
   Dimensions,
+  Keyboard,
+  KeyboardEvent,
+  PixelRatio,
   SafeAreaView,
   StyleSheet,
 } from "react-native";
@@ -27,33 +30,70 @@ const WebViewScreen = () => {
   const [navState, setNavState] = useState<WebViewNativeEvent>();
   const webviewRef = useRef<WebView>(null);
 
+  const [keyboardHeight, setKeyboardHeight] = useState<number>(0);
+
   const onMessage = async (event: WebViewMessageEvent) => {
     try {
       const message = JSON.parse(event.nativeEvent.data);
 
       console.log(message);
 
-      if (message.action === "getAlbum") {
-        let limit = 1;
-
-        switch (message.type) {
-          case "Blog":
-            limit = 10;
-            break;
-          case "Q&A":
-            limit = 5;
-            break;
-          case "Profile":
-          default:
-            limit = 1;
-        }
-
-        navigation.navigate("Album", { limit, webviewRef });
+      switch (message.action) {
+        case "getAlbum":
+          let limit = 1;
+          switch (message.type) {
+            case "Blog":
+              limit = 10;
+              break;
+            case "Q&A":
+              limit = 5;
+              break;
+            case "Profile":
+            default:
+              limit = 1;
+          }
+          navigation.navigate("Album", { limit, webviewRef });
+          break;
+        default:
+          break;
       }
     } catch (error) {
       console.error("onMessage Error:", error);
     }
   };
+
+  useEffect(() => {
+    function onKeyboardDidShow(e: KeyboardEvent) {
+      webviewRef.current?.postMessage(
+        JSON.stringify({
+          data: {
+            action: "keyboardHeightData",
+            height: PixelRatio.getPixelSizeForLayoutSize(
+              e.endCoordinates.height
+            ),
+          },
+        })
+      );
+      setKeyboardHeight(e.endCoordinates.height);
+    }
+
+    function onKeyboardDidHide() {
+      setKeyboardHeight(0);
+    }
+
+    const showSubscription = Keyboard.addListener(
+      "keyboardDidShow",
+      onKeyboardDidShow
+    );
+    const hideSubscription = Keyboard.addListener(
+      "keyboardDidHide",
+      onKeyboardDidHide
+    );
+    return () => {
+      showSubscription.remove();
+      hideSubscription.remove();
+    };
+  }, []);
 
   useEffect(() => {
     const cangoBack = navState?.canGoBack;
