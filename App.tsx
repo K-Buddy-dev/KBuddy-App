8f76ca9467d6421548e379018c7882bf881d5973
parkjongjoon7128 <whdwnsdk8111@naver.com>
2025-06-05 16:55:36 +0900

    feat: splash screen 구현중

--- App.tsx
+++ App.tsx
@@ -10,13 +10,21 @@ import {
   useNavigationContainerRef,
 } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { useEffect, useRef, useState } from "react";
-import { Alert, Platform } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Alert, Platform, View } from "react-native";
 import AlbumScreen from "./src/screens/AlbumScreen";
 import OnBoardingScreen from "./src/screens/OnBoardingScreen";
 import WebViewScreen from "./src/screens/WebViewScreen";
 
 const Stack = createStackNavigator<ROOT_NAVIGATION>();
+
+SplashScreen.preventAutoHideAsync();
+
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function App() {
   // Logic
@@ -26,6 +34,27 @@ export default function App() {
   const routeNameRef = useRef<string | null>(null);
 
   const [firstLaunch, setFirstLaunch] = useState<boolean | null>(null);
+  const [appIsReady, setAppIsReady] = useState<boolean>(false);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (error) {
+        console.warn(error);
+      } finally {
+        setAppIsReady(true);
+      }
+    };
+
+    prepare();
+  }, []);
 
   useEffect(() => {
     if (Platform.OS === "android") {
@@ -56,12 +85,16 @@ export default function App() {
   }, []);
 
   // View
-  if (firstLaunch === null) {
+  if (!appIsReady || firstLaunch === null) {
     return null;
   }
-  // 어플 실행이 처음인 경우
-  else if (firstLaunch === true) {
-    return (
+
+  return (
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      {/* 
+          어플 실행이 처음인 경우: Onboarding
+          어플 실행이 처음이 아닌 경우: WebView
+      */}
       <NavigationContainer
         ref={navigationRef}
         onReady={() => {
@@ -92,7 +125,7 @@ export default function App() {
         }}
       >
         <Stack.Navigator
-          initialRouteName="OnBoarding"
+          initialRouteName={firstLaunch ? "OnBoarding" : "WebView"}
           screenOptions={{ headerShown: false }}
         >
           <Stack.Screen name="WebView" component={WebViewScreen} />
@@ -100,49 +133,6 @@ export default function App() {
           <Stack.Screen name="OnBoarding" component={OnBoardingScreen} />
         </Stack.Navigator>
       </NavigationContainer>
-    );
-  }
-  // 어플 실행이 처음이 아닌 경우
-  else {
-    return (
-      <NavigationContainer
-        ref={navigationRef}
-        onReady={() => {
-          if (navigationRef.current) {
-            routeNameRef.current =
-              navigationRef.current.getCurrentRoute()?.name || "WebView";
-          }
-        }}
-        onStateChange={async () => {
-          if (!navigationRef.current) return;
-
-          const previousRouteName = routeNameRef.current;
-          const currentRoute = navigationRef.current.getCurrentRoute();
-
-          if (!currentRoute || !currentRoute.name) return;
-
-          const currentRouteName = currentRoute.name;
-
-          if (previousRouteName !== currentRouteName) {
-            const analytics = getAnalytics();
-
-            await logScreenView(analytics, {
-              screen_name: currentRouteName,
-              screen_class: currentRouteName,
-            });
-          }
-          routeNameRef.current = currentRouteName;
-        }}
-      >
-        <Stack.Navigator
-          initialRouteName="WebView"
-          screenOptions={{ headerShown: false }}
-        >
-          <Stack.Screen name="WebView" component={WebViewScreen} />
-          <Stack.Screen name="Album" component={AlbumScreen} />
-          <Stack.Screen name="OnBoarding" component={OnBoardingScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
-  }
+    </View>
+  );
 }
