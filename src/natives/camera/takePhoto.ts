f490dfbbc869d2d05792627318e02271f5c8bed6
parkjongjoon7128 <whdwnsdk8111@naver.com>
2025-06-17 16:06:53 +0900

    add: expo-image-manipulator 라이브러리 추가 및 이미지 리사이징 구현중

--- src/natives/camera/takePhoto.ts
+++ src/natives/camera/takePhoto.ts
@@ -1,3 +1,4 @@
+import * as ImageManipulator from "expo-image-manipulator";
 import * as ImagePicker from "expo-image-picker";
 
 export const takePhoto = async () => {
@@ -8,11 +9,19 @@ export const takePhoto = async () => {
   });
 
   if (!photo.canceled && photo.assets?.length > 0) {
-    const result = photo.assets.map((p) => {
-      return `data:${p.mimeType};base64,${p.base64}`;
-    });
+    const original_photo = photo.assets[0];
+    const resized_photo = await ImageManipulator.manipulateAsync(
+      original_photo.uri,
+      [{ resize: { width: 800 } }],
+      { compress: 0.5, format: ImageManipulator.SaveFormat.JPEG, base64: true }
+    );
 
-    return result;
+    return [`data:image/jpeg;base64,${resized_photo.base64}`];
+    // const result = photo.assets.map((p) => {
+    //   return `data:${p.mimeType};base64,${p.base64}`;
+    // });
+
+    // return result;
   }
 
   return null;
