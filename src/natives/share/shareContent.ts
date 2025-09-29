914cf5d34325148ca87186af8a97f59a097f8bad
parkjongjoon7128 <whdwnsdk8111@naver.com>
2025-09-30 00:10:25 +0900

    feat: 공유하기 crashlytics 추가

--- src/natives/share/shareContent.ts
+++ src/natives/share/shareContent.ts
@@ -1,11 +1,15 @@
+import crashlytics from "@react-native-firebase/crashlytics";
 import { Share } from "react-native";
 
 const shareContent = async (title: string, url: string, imageUrl: string) => {
   await Share.share({
     message: `${title} \n\n ${url} \n\n ${imageUrl}`,
   })
-    .then((res) => console.log("공유하기 성공: ", res))
-    .catch((err) => console.log("공유하기 실패: ", err));
+    .then((result) => console.log("공유하기 성공: ", result))
+    .catch((error) => {
+      crashlytics().recordError(error as Error);
+      console.log("공유하기 오류: ", error);
+    });
 };
 
 export default shareContent;
