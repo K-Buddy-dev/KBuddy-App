515c05868d99a2602867cdc978db04c1c2222205
parkjongjoon7128 <whdwnsdk8111@naver.com>
2025-11-08 22:56:46 +0900

    feat: Background 푸시 알림 데이터 로깅 추가

--- index.ts
+++ index.ts
@@ -1,9 +1,22 @@
 import { registerRootComponent } from "expo";
 
+import messaging from "@react-native-firebase/messaging";
 import "expo-dev-client";
+import { Platform } from "react-native";
 import App from "./App";
 
 // Background message handler
+messaging().setBackgroundMessageHandler(async (notification) => {
+  Platform.OS === "ios"
+    ? console.log(
+        "Background notification on ios: ",
+        JSON.stringify(notification, null, 3)
+      )
+    : console.log(
+        "Background notification on Android: ",
+        JSON.stringify(notification, null, 3)
+      );
+});
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
