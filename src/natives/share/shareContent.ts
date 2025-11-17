e2828ed88d00b07ee92286d72549b26eb2d9ea2e
parkjongjoon7128 <whdwnsdk8111@naver.com>
2025-11-18 02:44:56 +0900

    add: react-native-share- 라이브러리 추가

--- src/natives/share/shareContent.ts
+++ src/natives/share/shareContent.ts
@@ -1,15 +1,7 @@
-import crashlytics from "@react-native-firebase/crashlytics";
-import { Share } from "react-native";
-
-const shareContent = async (title: string, url: string, imageUrl: string) => {
-  await Share.share({
-    message: `${title} \n\n ${url} \n\n ${imageUrl}`,
-  })
-    .then((result) => console.log("공유하기 성공: ", result))
-    .catch((error) => {
-      crashlytics().recordError(error as Error);
-      console.log("공유하기 오류: ", error);
-    });
-};
+const shareContent = async (
+  title: string,
+  description: string,
+  url: string
+) => {};
 
 export default shareContent;
